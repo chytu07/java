f7432c4dcaa70fc15f445d173726156b33c75628
chaitanya <chaitanya.thulasaku@spectraforce.com>
2023-06-07 13:06:57 +0530

    added git ignore

--- src/generics/GenArrays.java
+++ src/generics/GenArrays.java
@@ -1,3 +1,5 @@
+package generics;
+
 // Generics and arrays.
 class Gen<T extends Number> {
     T ob;
@@ -13,10 +15,10 @@ class Gen<T extends Number> {
 class GenArrays {
     public static void main(String args[]) {
         Integer n[] = { 1, 2, 3, 4, 5 };
-        Gen<Integer> iOb = new Gen<Integer>(50, n);
+        Gen1<Integer> iOb = new Gen1<Integer>(50, n);
 // Can't create an array of type-specific generic references.
 // Gen<Integer> gens[] = new Gen<Integer>[10]; // Wrong!
 // This is OK.
-        Gen<?> gens[] = new Gen<?>[10]; // OK
+        Gen1<?> gens[] = new Gen1<?>[10]; // OK
     }
 }   
